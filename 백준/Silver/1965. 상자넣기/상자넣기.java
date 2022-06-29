157b581f9c5575a8221650eb031b58cf99c3cfd3
eunkyung-kim-99 <64150747+eunkyung-kim-99@users.noreply.github.com>
2022-06-29 23:34:55 +0900

    [Silver II] Title: 상자넣기, Time: 152 ms, Memory: 14564 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bf.readLine());
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int[] arr = new int[N];
+		int[] dp = new int[N];
+		int max = 0;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
+	}
+}
