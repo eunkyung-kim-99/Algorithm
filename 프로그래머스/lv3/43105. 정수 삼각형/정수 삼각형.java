750684ab0defdd74068bbb823a800291a6105ec9
eunkyung-kim-99 <64150747+eunkyung-kim-99@users.noreply.github.com>
2022-10-12 05:15:27 +0900

    [level 3] Title: 정수 삼각형, Time: 14.28 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int[][] dp = new int[triangle.length][triangle.length];
+        for(int i = 0; i< triangle[triangle.length - 1].length; i++){
+            dp[triangle.length - 1][i] = triangle[triangle.length - 1][i];
+        }
+        for(int i = triangle.length - 1; i > 0; i--){
+            for(int j = 0; j < triangle[i].length; j++){
+                if(j - 1 >= 0){
+                    dp[i - 1][j - 1] = Math.max(dp[i - 1][j - 1], triangle[i - 1][j - 1] + dp[i][j]);
+                }
+                if(j == triangle[i].length - 1) continue;
+                dp[i - 1][j] = Math.max(dp[i - 1][j], triangle[i - 1][j] + dp[i][j]);                
+            }
+            // print(dp);
+        }                          
+        return dp[0][0];
+    }
+    public void print(int[][] dp){
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
